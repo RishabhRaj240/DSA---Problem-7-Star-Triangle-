8b039c83e25c0fa53b665f814d9f3b20c62a21f6
Rishab Raj <rishabhraj240405@gmail.com>
2026-06-22 21:30:35 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 void nStarTriangle(int n) {
     for (int i = 1; i <= n; i++) {
 	    // Spaces
-	    for (int j = 1; j <= n - i; j++) {
+	    for (int j = 1; j <= n - i - 1; j++) {
 		    cout << " ";
 	    }
 	    // Stars
-	    for (int k = 0; k < 2 * i - 1; k++) {
+	    for (int j = 0; j < 2 * i - 1; j++) {
 		    cout << "*";
+	    }
+		// Spaces
+	    for (int j = 1; j <= n - i - 1; j++) {
+		    cout << " ";
 	    }
 	    cout << endl;
     }
